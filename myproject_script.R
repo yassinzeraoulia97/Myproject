c229b7726341e671c828ced4f1ca6f433a08fea8
yassinzeraoulia97 <zeraouliayassin@gmail.com>
2021-05-19 15:44:47 +0200

    new project

---
+++ myproject_script.R
@@ -0,0 +1,21 @@
+#Loading the data set
+
+
+library(readr)
+healthcare_dataset_stroke_data <- read_csv("healthcare-dataset-stroke-data.csv")
+
+
+#exploration of the data set and rename the data set
+
+
+mydata <- healthcare_dataset_stroke_data
+
+nrow(mydata)
+ncol(mydata)
+head(mydata)
+
+#remove unnecessary columns 
+
+newdata <- mydata[, c("gender", "age", "hypertension", "heart_disease", "work_type", "Residence_type", "avg_glucose_level", "bmi", "smoking_status")]
+
+head(newdata)
